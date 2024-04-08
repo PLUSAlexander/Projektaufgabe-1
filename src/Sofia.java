9cb5630b71f9da49054561b332f275350a6cc0ff
Alexander Neubacher <alexander.neubacher@stud.plus.ac.at>
2024-04-08 09:33:25 +0200

    sofia v2h

--- src/Sofia.java
+++ src/Sofia.java
@@ -1,4 +1,3 @@
-import javax.print.DocFlavor;
 import java.sql.*;
 import java.util.*;
 
@@ -18,8 +17,6 @@ public class Sofia {
     static Map<String, Map<Integer, Integer>> intAttributeRegister = new HashMap<>();      //register for each int attribute
 
 
-
-
     public static void main(String[] args) throws Exception {
         try {
             con = DriverManager.getConnection(url, user, pwd);
@@ -28,133 +25,28 @@ public class Sofia {
             System.out.println("Failed to Connect: " + e.getMessage());
         }
 
-       showConnectionAndSQL();
-
+        showConnectionAndSQL();
         //generate(30, 0.2, 7);
-        //h2v("h");
-        //v2h("v");
+        generate(7,0.0,4);
+        h2v("h");
+        v2h("hh2v");
     }
-
-    // TODO: implement V2H
-    // nicht Select oid, a1 from V where key = a2 and val b
-    // sondern: Select oid, a1 from H where a2 = b
-    // insert into v2h (Ai) Values (attWert);
-
-
-
-    public static void v2h(String tableName) throws SQLException {
-        //Delete if already exists
-        Statement stDrop = con.createStatement();
-        String sqlDrop = "DROP Table if exists v2h;";
-        stDrop.execute(sqlDrop);
-
-        //Create horizontal table
-        StringBuilder sb = new StringBuilder();
-        sb.append("CREATE TABLE v2h (Oid int, ");
-        ArrayList<String> attributeNames = new ArrayList<>();
-
-        Statement stm = con.createStatement();
-        StringBuilder sbuilder = new StringBuilder();
-        sbuilder.append("SELECT distinct Key FROM " + tableName);
-        ResultSet rs = stm.executeQuery(sbuilder.toString());
-        while (rs.next()) {
-            attributeNames.add(rs.getString(1));
-        }
-        Collections.sort(attributeNames);
-
-        for (int i = 0; i < attributeNames.size(); i++) {
-            String att = attributeNames.get(i);
-            if (i % 2 == 0) {
-                sb.append(att + " int");
-            } else {
-                sb.append(att + " varchar(255)");
-            }
-            if (i < attributeNames.size() - 1) {
-                sb.append(", ");
-            }
-        }
-        sb.append(");");
-        System.out.printf(sb.toString());
-        Statement st = con.createStatement();
-        st.execute(sb.toString());
-
-
-
-
-        //Fill horizontal table
-        Statement getOid = con.createStatement();
-        StringBuilder stb = new StringBuilder();
-        stb.append("SELECT distinct Oid from " + tableName);
-        ResultSet rset = getOid.executeQuery(stb.toString()); // number of tuples in horizontal table
-        List<String> oids = new ArrayList<>();
-        while (rset.next()){
-            String oid = rset.getString(1);
-            oids.add(oid);
-        }
-
-        HashMap<String, HashMap<String, String>> attributes = new HashMap<>(); //map to safe every value of each attribute
-
-        for (int i = 0;  i < attributeNames.size(); i++) {
-            StringBuilder builder = new StringBuilder();
-            String attName = "a" + i;
-            Statement getAttValues = con.createStatement();
-
-            builder.append("Select Oid, val From " + tableName + "Where key = 'a" + i + "'");
-            ResultSet results = getAttValues.executeQuery(getAttValues.toString());
-            HashMap<String, String> attributeValues = new HashMap<>();
-
-            while (results.next()) {
-
-                attributeValues.put(results.getString(1), results.getString((2))); //To Do
-            }
-            attributes.put(attName, attributeValues);
-        }
-
-        int Oid = 1;
-
-        for(int i = 0; i<oids.size(); i++) {
-            StringBuilder build = new StringBuilder();
-            build.append("INSERT into " + tableName + "VALUES(");
-
-            for(int j = 0; j < attributes.size() ;j++) {
-                String attName = "a" + i;
-                HashMap<String, String> attributeVals = attributes.get(attName);
-
-                if(attributeVals.containsKey(Oid)){
-                    String value = attributeVals.get(Oid);
-                    build.append(value);
-                }
-                else {
-                    build.append("NULL");
-                }
-
-                if (i < oids.size() - 1) {
-                    build.append(", ");
-                }
-            }
-            sb.append(");");
-            Statement insertAttValues = con.createStatement();
-            insertAttValues.execute(build.toString());
-        }
-    }
-
-
-
 
 
 
     //Phase 1
 
     public static void generate(int num_tuples, double sparsity, int num_attributes) throws Exception {
-        //delete H if exists
+        //delete H if it already exists
         Statement st = con.createStatement();
         String sql = "DROP Table if exists H;";
         st.execute(sql);
 
-        //create Table with String and int Attributes
+
+        //create table with String and int attributes
         String[] attributes = new String[num_attributes];
         for (int i = 0; i < num_attributes; i++) {
-            attributes[i] = (i % 2 == 0)? "int" : "String";
+            attributes[i] = (i % 2 == 0) ? "int" : "String";
         }
 
         StringBuilder sb = new StringBuilder("CREATE TABLE H (");
@@ -174,7 +66,8 @@ public class Sofia {
         Statement createTable = con.createStatement();
         createTable.execute(sb.toString());
 
-        //Fill Table
+
+        //fill table
         StringBuilder insert = new StringBuilder("INSERT INTO H VALUES ");
         int oid = 1;
 
@@ -191,7 +84,7 @@ public class Sofia {
                         String att = "a" + j;
                         if (attributes[j].equals("String")) {
                             String valueString = getStringValue(att, stringValues);
-                            insert.append("'" + valueString + "'");
+                            insert.append("'").append(valueString).append("'");
                         } else {
                             int valueInt = getIntValue(att, intValues);
                             insert.append(valueInt);
@@ -211,10 +104,11 @@ public class Sofia {
         Statement insertInto = con.createStatement();
         insertInto.execute(insert.toString());
 
-        //Insert special case with all attributes NULL
+
+        //insert special case with all attributes NULL
         StringBuilder insertSpecial = new StringBuilder("INSERT INTO H VALUES (");
 
-        for(int i = 0; i < num_attributes; i++) {
+        for (int i = 0; i < num_attributes; i++) {
             if (i == 0)
                 insertSpecial.append(oid);
             else
@@ -231,8 +125,9 @@ public class Sofia {
         System.out.println("Table created and filled.");
     }
 
-    public static String getStringValue(String att, List<String> valueList) throws Exception{
-        if(valueList.isEmpty())
+
+    public static String getStringValue(String att, List<String> valueList) throws Exception {
+        if (valueList.isEmpty())
             throw new Exception("All values already used 5 times in attribute " + att);
 
         String val = valueList.get(RANDOM.nextInt(valueList.size()));   //choose random value from list
@@ -243,7 +138,7 @@ public class Sofia {
         stringValueCounters.put(val, updatedCounter);                        //update counter in map
         stringAttributeRegister.put(att, stringValueCounters);               //update register
 
-        if(updatedCounter <= 5)                                              //check if it would be the fifth time, if so choose other value
+        if (updatedCounter <= 5)                                              //check if it would be the fifth time, if so choose other value
             return val;
         else {
             List<String> updatedValueList = new ArrayList<>(valueList);
@@ -253,8 +148,8 @@ public class Sofia {
     }
 
 
-    public static int getIntValue(String att, List<Integer> valueList) throws Exception{
-        if(valueList.isEmpty())
+    public static int getIntValue(String att, List<Integer> valueList) throws Exception {
+        if (valueList.isEmpty())
             throw new Exception("All values used more than 5 times in attribute " + att);
 
         int val = valueList.get(RANDOM.nextInt(valueList.size()));   //choose random value from list
@@ -265,7 +160,7 @@ public class Sofia {
         intValueCounters.put(val, updatedCounter);                        //update counter in map
         intAttributeRegister.put(att, intValueCounters);                  //update register
 
-        if(updatedCounter <= 5)                                           //check if it would be the fifth time, if so choose other value
+        if (updatedCounter <= 5)                                           //check if it would be the fifth time, if so choose other value
             return val;
         else {
             List<Integer> updatedValueList = new ArrayList<>(valueList);
@@ -274,17 +169,18 @@ public class Sofia {
         }
     }
 
+
     public static void showConnectionAndSQL() throws SQLException {
         Statement st = con.createStatement();
         String delete = "DROP Table if exists showCon;";
         st.execute(delete);
 
         Statement stm = con.createStatement();
-        String create = "CREATE TABLE showCon (Oid INT, a1 VARCHAR(5));";
+        String create = "CREATE TABLE showCon(Oid INT, a1 VARCHAR(5));";
         stm.execute(create);
 
         Statement stmnt = con.createStatement();
-        String insert = "INSERT INTO showCon VALUES (1, 'abc');";
+        String insert = "INSERT INTO showCon VALUES(1, 'abc');";
         stmnt.execute(insert);
 
         System.out.println("Table created and filled.");
@@ -296,18 +192,22 @@ public class Sofia {
     //Phase 2
 
     public static void h2v(String tableName) throws SQLException {
+        //delete h2v and h2v_temp if they already exist
         Statement stDrop = con.createStatement();
         String sqlDrop = "DROP Table if exists h2v_temp;";
         stDrop.execute(sqlDrop);
         Statement orginalDrop = con.createStatement();
-        String sqlOrginalDrop = "DROP Table if exists h2v;";
+        String sqlOrginalDrop = "DROP Table if exists " + tableName + "H2V;";
         orginalDrop.execute(sqlOrginalDrop);
 
 
+        //create vertical table
         Statement stCreateVertical = con.createStatement();
         String sqlCreateVertical = "CREATE TABLE H2V_temp (Oid int, Key varchar(5), Val varchar(255));";
         stCreateVertical.execute(sqlCreateVertical);
 
+
+        //get data from table
         DatabaseMetaData metaData = con.getMetaData();
         ResultSet resultSet = metaData.getColumns(null, null, tableName, null);
 
@@ -319,10 +219,11 @@ public class Sofia {
             attributeTypes.put(columnName, dataType);
         }
 
+
+        //fill table
         for (String s : attributeTypes.keySet()) {
             if (!s.equals("oid")) {
                 String sql = "SELECT oid," + s + " FROM " + tableName + " WHERE " + s + " is not null;";
-                System.out.println(sql);
                 Statement st = con.createStatement();
                 ResultSet rs1 = st.executeQuery(sql);
                 while (rs1.next()) {
@@ -337,13 +238,127 @@ public class Sofia {
         }
 
 
-        String sqlSortTable = "CREATE TABLE H2V AS SELECT * FROM H2V_temp ORDER BY Oid ASC, key;";
+        //sort table
+        String sqlSortTable = "CREATE TABLE " + tableName + "H2V AS SELECT * FROM H2V_temp ORDER BY Oid ASC, key;";
         Statement stSortTable = con.createStatement();
         stSortTable.execute(sqlSortTable);
 
         String sqlDropTemp = "DROP TABLE H2V_temp;";
         Statement stDropTemp = con.createStatement();
         stDropTemp.execute(sqlDropTemp);
+
+        System.out.println("Successfully converted to vertical.");
     }
 
+
+    public static void v2h(String tableName) throws SQLException {
+        //delete Horizontal table and v2helper if they already exist
+        Statement stmDrop = con.createStatement();
+        String sqlDrop = "DROP Table if exists " + tableName + "V2H;";
+        stmDrop.execute(sqlDrop);
+        Statement stDrop = con.createStatement();
+        String stringDrop = "DROP Table if exists v2helper;";
+        stDrop.execute(stringDrop);
+
+
+        //get attribute names
+        Statement stmGetAttNames = con.createStatement();
+        String getAttNames = "SELECT distinct Key FROM " + tableName;
+        ArrayList<String> attributeNames = new ArrayList<>();
+        ResultSet rs = stmGetAttNames.executeQuery(getAttNames);
+        while (rs.next()) {
+            attributeNames.add(rs.getString(1));
+        }
+        Collections.sort(attributeNames);
+
+
+        //create helper table for joins and fill initially with all oids (as integers)
+        Statement stmJoinHelper = con.createStatement();
+        String joinHelper = "CREATE table v2helper (oid int);";
+        stmJoinHelper.execute(joinHelper);
+
+        Statement stMaxOid = con.createStatement();
+        String maxOidString = "SELECT MAX(oid) AS max_value FROM " + tableName + ";";
+        ResultSet rsMaxOid = stMaxOid.executeQuery(maxOidString);
+        int maxOid = 0;
+        while (rsMaxOid.next()) {
+            maxOid = Integer.parseInt(rsMaxOid.getString(1));}
+
+        for (int i = 1; i <= maxOid; i++) {
+            Statement stmInsertHelper = con.createStatement();
+            String insertHelper = "insert into v2helper values(" + i + ");";
+            stmInsertHelper.execute(insertHelper);
+        }
+
+
+        //create temporary table for each attribute
+        int attCounter = 0;
+
+        for (String attName : attributeNames) {
+            Statement stmGetAttOid = con.createStatement();       //get oids from all tuples that have the attribute
+            String getAttOid = "SELECT oid FROM " + tableName + " WHERE key = '" + attName + "';";
+            ResultSet resultOid = stmGetAttOid.executeQuery(getAttOid);
+
+            Statement stmGetAttVal = con.createStatement();      //get values from all tuples that have the attribute
+            String getAttVal = "SELECT val FROM " + tableName + " WHERE key = '" + attName + "';";
+            ResultSet resultVal = stmGetAttVal.executeQuery(getAttVal);
+
+            Statement stmCreateHelper = con.createStatement();
+            String createHelper;
+            if(attCounter % 2 == 0){       //adjust type (String or Integer)
+                createHelper = "CREATE TEMPORARY TABLE " + attName + " (oid int, " + attName + " varchar(255));";
+            }
+            else {
+                createHelper = "CREATE TEMPORARY TABLE " + attName + " (oid int, " + attName + " int);";
+            }
+            stmCreateHelper.execute(createHelper);
+
+            while (resultOid.next() && resultVal.next()) {
+                Statement stmFillHelper = con.createStatement();
+                String oidString = resultOid.getString(1);
+                int oid = Integer.parseInt(oidString);
+                String valString = resultVal.getString(1);
+                if(attCounter % 2 == 0){
+                    String fillHelper = "insert into " + attName + "(oid, " + attName + ") values(" + oid + ", '" + valString + "');";
+                    stmFillHelper.executeUpdate(fillHelper);
+                }
+                else {
+                    int val = Integer.parseInt(valString);
+                    String fillHelper = "insert into " + attName + "(oid, " + attName + ") values(" + oid + ", " + val + ");";
+                    stmFillHelper.executeUpdate(fillHelper);
+                }
+            }
+            attCounter++;
+
+
+            //join attribute with the helper and update the helper
+            Statement stmCreateV3helper = con.createStatement();
+            String createV3helper = "CREATE TABLE v3helper AS (SELECT * FROM v2helper);";
+            stmCreateV3helper.executeUpdate(createV3helper);
+
+            Statement stmDeleteOldV2helper = con.createStatement();
+            String deleteOldV2helper = "DROP Table if exists v2helper;";
+            stmDeleteOldV2helper.executeUpdate(deleteOldV2helper);
+
+            Statement stmJoinAttOfTuple = con.createStatement();
+            String joinAttOfTuple = "CREATE TABLE v2helper AS (SELECT v3helper.*, " + attName + "." + attName + " FROM v3helper LEFT JOIN " + attName + " ON v3helper.oid = " + attName + ".oid);";
+            stmJoinAttOfTuple.executeUpdate(joinAttOfTuple);
+
+            Statement stmDeleteOldV3helper = con.createStatement();
+            String deleteOldV3helper = "DROP Table if exists v3helper;";
+            stmDeleteOldV3helper.executeUpdate(deleteOldV3helper);
+        }
+
+
+        //sort table
+        String sqlSortTable = "CREATE TABLE " + tableName + "V2H AS SELECT * FROM v2helper ORDER BY oid ASC;";
+        Statement stSortTable = con.createStatement();
+        stSortTable.execute(sqlSortTable);
+
+        String sqlDropTemp = "DROP TABLE v2helper;";
+        Statement stDropTemp = con.createStatement();
+        stDropTemp.execute(sqlDropTemp);
+
+        System.out.println("Successfully converted to horizontal.");
+    }
 }
